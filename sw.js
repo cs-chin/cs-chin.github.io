16885a866e09111c4bd41e4a01e8ad1f78fd4597
chin-mac <chin@cyberstep.com>
2020-02-18 17:26:38 +0900

    調整

--- sw.js
+++ sw.js
@@ -1,39 +1,43 @@
-const version = 'v1';
+self.addEventListener('fetch', function(e) {
+  // ここは空でもOK
+})
 
-// インストール時にキャッシュする
-self.addEventListener('install', (event) => {
-console.log('service worker install ...');
+// const version = 'v1';
 
-  // キャッシュ完了までインストールが終わらないように待つ
-  // event.waitUntil(
-  // caches.open('v1').then((cache) => {
-  //   return cache.addAll([
-  //     '/',
-  //   ]);
-  // })
-  // );
-});
+// // インストール時にキャッシュする
+// self.addEventListener('install', (event) => {
+// console.log('service worker install ...');
+
+//   // キャッシュ完了までインストールが終わらないように待つ
+//   // event.waitUntil(
+//   // caches.open('v1').then((cache) => {
+//   //   return cache.addAll([
+//   //     '/',
+//   //   ]);
+//   // })
+//   // );
+// });
 
 
-self.addEventListener('activate', (event) => {
-  console.info('activate', event);
-});
+// self.addEventListener('activate', (event) => {
+//   console.info('activate', event);
+// });
 
-self.addEventListener('fetch', function(event) {
-  console.log('fetch', event.request.url);
+// self.addEventListener('fetch', function(event) {
+//   console.log('fetch', event.request.url);
 
-  event.respondWith(
-    // リクエストに一致するデータがキャッシュにあるかどうか
-    caches.match(event.request).then(function(cacheResponse) {
-      // キャッシュがあればそれを返す、なければリクエストを投げる
-      return cacheResponse || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
-          // レスポンスをクローンしてキャッシュに入れる
-          cache.put(event.request, response.clone());
-          // オリジナルのレスポンスはそのまま返す
-          return response;
-        });  
-      });
-    })
-  );
-});
+//   event.respondWith(
+//     // リクエストに一致するデータがキャッシュにあるかどうか
+//     caches.match(event.request).then(function(cacheResponse) {
+//       // キャッシュがあればそれを返す、なければリクエストを投げる
+//       return cacheResponse || fetch(event.request).then(function(response) {
+//         return caches.open('v1').then(function(cache) {
+//           // レスポンスをクローンしてキャッシュに入れる
+//           cache.put(event.request, response.clone());
+//           // オリジナルのレスポンスはそのまま返す
+//           return response;
+//         });  
+//       });
+//     })
+//   );
+// });
