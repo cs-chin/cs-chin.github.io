ca86ef83abf852beab4694cf000dbca4528ca8ff
chin-mac <chin@cyberstep.com>
2020-02-19 17:45:08 +0900

    調整

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
   
-  import {registerRoute} from 'workbox-routing';
-  import {NetworkFirst} from 'workbox-strategies';
+  // import {registerRoute} from 'workbox-routing';
+  // import {NetworkFirst} from 'workbox-strategies';
 
-  registerRoute(
-    /\.js$/,
-    new NetworkFirst()
-  );
+  // registerRoute(
+  //   /\.js$/,
+  //   new NetworkFirst()
+  // );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
