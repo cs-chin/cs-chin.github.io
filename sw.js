f9b911cc8024c4bac2d08b86b23ab784d718b99b
chin-mac <chin@cyberstep.com>
2020-02-19 17:30:06 +0900

    registerRoute

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
+
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  
+  import {registerRoute} from 'workbox-routing';
+  import {NetworkFirst} from 'workbox-strategies';
+
+  registerRoute(
+    /\.js$/,
+    new NetworkFirst()
+  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
